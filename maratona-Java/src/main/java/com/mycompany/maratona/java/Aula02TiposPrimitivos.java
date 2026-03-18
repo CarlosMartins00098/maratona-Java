de9b47f5cc4bbc44084439b947e79b5d6b1b9ca7
CarlosMartins00098 <carlosmartins00098@gmail.com>
2026-03-18 14:36:45 -0300

    aula 2 tipos primitivos

---
+++ maratona-Java/src/main/java/com/mycompany/maratona/java/Aula02TiposPrimitivos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.maratona.java;
+
+/**
+ *
+ * @author carlos
+ */
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, byte, short, long, boolean
+        int idade = 10; 
+        System.out.println("a idade é "+idade);
+    }
+}
