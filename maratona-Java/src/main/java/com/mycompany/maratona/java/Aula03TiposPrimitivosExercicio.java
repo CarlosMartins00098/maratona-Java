043b98831418d5678c4117d270ee12cccc7be433
CarlosMartins00098 <carlosmartins00098@gmail.com>
2026-03-18 14:48:36 -0300

    configurações basicas

---
+++ maratona-Java/src/main/java/com/mycompany/maratona/java/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.maratona.java;
+
+/**
+ *
+ * @author carlos
+ */
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, byte, short, long, boolean
+        int idade = 10; 
+        long numGrande = (long) 155.23;
+        double salDouble = 2800.80;
+        float salFloat = (float) 2500.8D;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+        
+        System.out.println("a idade é "+idade+" anos");
+        System.out.println(falso);
+        System.out.println("char "+caractere);
+        System.out.println(numGrande);
+        
+    }
+}
