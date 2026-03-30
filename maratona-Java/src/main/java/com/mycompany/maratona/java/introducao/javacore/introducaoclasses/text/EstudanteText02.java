1f7a3bf6b02ddaaa11ca35a3c5f9c2435557c7bf
CarlosMartins00098 <carlosmartins00098@gmail.com>
2026-03-30 06:58:35 -0300

    add oo pt2 EstudanteText02

--- maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/introducaoclasses/text/EstudanteText02.java
+++ maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/introducaoclasses/text/EstudanteText02.java
@@ -13,10 +13,18 @@ import com.mycompany.maratona.java.introducao.javacore.Aintroducaoclasses.domini
 public class EstudanteText02 {
     public static void main(String[] args) {
         Estudante estudante = new Estudante();
+        Estudante estudante2 = new Estudante();
         
+        estudante.nome = "Sanji";
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
         System.out.println(estudante.nome);
+        
+        System.out.println("------------------");
+        
+        System.out.println(estudante2.idade);
+        System.out.println(estudante2.sexo);
+        System.out.println(estudante2.nome);
         
     }
 }
