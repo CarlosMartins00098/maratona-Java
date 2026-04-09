d2392f2d92e05a7a5390231444369ebd9c54254c
CarlosMartins00098 <carlosmartins00098@gmail.com>
2026-04-09 10:38:01 -0300

    fix rename CalculadoraTest03

--- maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/BintroducaoMetodos/text/CalculadoraTest03.java
+++ maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/BintroducaoMetodos/text/CalculadoraTest03.java
@@ -2,7 +2,7 @@ package com.mycompany.maratona.java.introducao.javacore.BintroducaoMetodos.text;
 
 import com.mycompany.maratona.java.introducao.javacore.BintroducaoMetodos.dominio.Calculadora;
 
-public class CaladoraTest03 {
+public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         double res = calculadora.divideDoisNumeros(20, 0);
