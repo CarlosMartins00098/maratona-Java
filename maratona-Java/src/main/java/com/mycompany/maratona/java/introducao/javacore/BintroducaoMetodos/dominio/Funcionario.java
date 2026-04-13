8ea83b68f88a902a8fcf40def7ee5607de80f95d
CarlosMartins00098 <carlosmartins00098@gmail.com>
2026-04-13 11:26:19 -0300

    add set & get funcionario

--- maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/BintroducaoMetodos/dominio/Funcionario.java
+++ maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/BintroducaoMetodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package com.mycompany.maratona.java.introducao.javacore.BintroducaoMetodos.dominio;
 
 public class Funcionario {
-    public String nome = null;
-    public int idade = 0;
-    public double[] salario = new double[3];
+    private String nome = null;
+    private int idade = 0;
+    private double[] salario = new double[3];
+    private double media;
     
     public void imprimir(){
         System.out.println("Nome: "+this.nome);
@@ -25,5 +26,37 @@ public class Funcionario {
         }
         System.out.println("a média salárial é: "+media);
         
+        
+        
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double[] salario) {
+        this.salario = salario;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    
 }
--- maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/BintroducaoMetodos/text/FuncionarioTest01.java
+++ maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/BintroducaoMetodos/text/FuncionarioTest01.java
@@ -6,13 +6,11 @@ public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
         
-        funcionario.nome = "Carlos";
-        funcionario.idade = 22;
-        funcionario.salario[0] = 50;
-        funcionario.salario[1] = 70;
-        funcionario.salario[2] = 1650;
-        
+        funcionario.setNome("Carlos");
+        funcionario.setIdade(22);
+        funcionario.setSalario(new double[]{50, 70,1650});
         funcionario.imprimir();
+        System.out.println("média: "+funcionario.getMedia());
         
     }
 }
