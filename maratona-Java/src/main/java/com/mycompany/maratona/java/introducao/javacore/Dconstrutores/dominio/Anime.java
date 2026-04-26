5da6a16232250653ef70263be3384c2e18b482f9
CarlosMartins00098 <carlosmartins00098@gmail.com>
2026-04-26 12:40:17 -0300

    add Dconstrutore anime pt2

--- maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/Dconstrutores/dominio/Anime.java
+++ maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/Dconstrutores/dominio/Anime.java
@@ -5,25 +5,33 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     // criar um construtor dentro da classe, ele executa primeiro 
     public Anime(String nome, String tipo, int episodios, String genero){
+        this();// o this precisa ser a primeira linha do código
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
     
-    // sobrecarga de contrutores
-    public Anime(){
-        
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio){
+        this(nome, tipo, episodios, genero);// o this precisa ser a primeira linha do código
+        this.estudio = estudio;
+
     }
     
+    // sobrecarga de contrutores
+    public Anime(){
+        System.out.println("bla bla bla");
+    }
     
     public void imprime(){
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.nome);
+        System.out.println(this.estudio);
     }
 
     public String getNome() {
