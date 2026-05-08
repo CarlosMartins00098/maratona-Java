d8cb0f685d876c60742b95213ba52ba625e6dea0
CarlosMartins00098 <carlosmartins00098@gmail.com>
2026-05-08 11:00:35 -0300

    63 - Orientação Objetos - Bloco de inicialização estático

---
+++ maratona-Java/src/main/java/com/mycompany/maratona/java/introducao/javacore/FmodificadorEstatico/dominio/Anime.java
@@ -0,0 +1,36 @@
+package com.mycompany.maratona.java.introducao.javacore.Eblocosinicializacao.domain;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+    // 1 - Alocado espaço em memória pro objeto
+    // 2 - Cada atributo de classe é criado e inicializado com valores default ou o que for passado
+    // 3 - Bloco de inicialização é executado 
+    // 4 - Construor é Executado
+    {
+        episodios = new int [100];
+        
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+    
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    
+    public Anime() {
+ 
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+    
+    
+}
